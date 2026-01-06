202dfd421396217516479ac60531070639848181
pinkcutieee <ririariii15@gmail.com>
2026-01-06 12:41:39 +0800

    Try import

--- src/components/Sparkles.jsx
+++ src/components/Sparkles.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
-import { cn } from "../../lib/utils";
+import { cn } from "../utils";
 
 export const SparklesCore = ({
   background = "transparent",
